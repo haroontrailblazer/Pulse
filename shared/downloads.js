3a858e848a6beaba04ce1274b86a711324e349ae
Haroon K M <haroonint144@gmail.com>
2026-09-14 03:24:57 +0530

    Hide browser scrollbars

--- shared/downloads.js
+++ shared/downloads.js
@@ -1,4 +1,4 @@
-export const releaseVersion = "1.0.7";
+export const releaseVersion = "1.0.8";
 const releaseBase = `https://pulse-status-zeta.vercel.app/downloads/v${releaseVersion}`;
 export const downloads = {
   windows: `${releaseBase}/Pulse-${releaseVersion}-Windows.exe`,
